36082db7268dd12b4350602f8f04d65ee9e0ca76
krmrhnm <krmrhnm130@gmail.com>
2026-04-28 13:59:49 +0900

    2026/04/28/13:59

--- Assets/Scripts/ScrollManager.cs
+++ Assets/Scripts/ScrollManager.cs
@@ -3,22 +3,19 @@ using UnityEngine;
 public class ScrollManager : MonoBehaviour
 {
     public int Stage=1;//stagemanager“™‚Ìscript‚ÅŠÇ—‚·‚éê‡‚Í‚»‚¿‚ç‚ğQÆ‚·‚é
-    public GameObject[] Stage1;
+    public GameObject[] Stage1;//Inspector‚ÅStage‚ÌPrefab‚ğ’Ç‰Á‚·‚é
     public GameObject[] Stage2;
     public GameObject[] Stage3;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
-
-    // Update is called once per frame
     void Update()
     {
         switch(Stage){
             default:
             case 1:
-                for(int i = 0; i < Stage1.Length; ++i)
+                for(int i = 0; i < Stage1.Length; ++i)//¡‚Í0~4‚ğ‡‚Éo‚µ‚Ä‚é
                 {
                     if(!GameObject.Find("Stage1_"+i)&&!GameObject.Find("Stage1_"+i+"(Clone)"))Instantiate(Stage1[i],new Vector3(i,0f,0f),Quaternion.identity);
                 }
--- Assets/Scripts/StageScript.cs
+++ Assets/Scripts/StageScript.cs
@@ -3,19 +3,16 @@ using UnityEngine;
 public class StageScript : MonoBehaviour
 {
     GameObject Camera;//プレハブ化するのでプライベート
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Camera = GameObject.Find("Main Camera");//名前指定なので変えると動かなくなる
         transform.parent = GameObject.Find("ScrollManager").transform;//上記と同様
     }
-
-    // Update is called once per frame
     void Update()
     {
         float pos1=this.transform.position.z;
         float pos2=Camera.transform.position.z;
         transform.Translate(new Vector3(0f,0f,-1f) * 10f*Time.deltaTime);
-        if (pos1 <= pos2) Destroy(this.gameObject);
+        if (pos1 <= pos2) Destroy(this.gameObject);//Cameraの後ろに行ったらこれのobjectを破壊する
     }
 }
