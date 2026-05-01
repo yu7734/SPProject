ef98a43d11b5594cebb8f1a9358253a4c2738654
lime <utida7497@gmail.com>
2026-05-01 11:09:20 +0900

    Add files via upload

--- EnemyManager.cs
+++ EnemyManager.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyManager : MonoBehaviour
@@ -18,8 +19,10 @@ public class EnemyManager : MonoBehaviour
     public float enemyBulletForce = 3f;
     
     [Header("突撃にかかわる変数")]
+    [Tooltip("最初の停止時間（秒）")]
+    public float stopTime = 1;
     [Tooltip("プレイヤーを見ている時間（秒）")]
-    public float lookAtTime = 1;
+    public float lookAtTime = 2;
     [Tooltip("敵の突撃速度")]
     public float enemyRushSpeed = 3f;
     [Tooltip("敵をプレイヤー通過後に非表示にするまでの距離")]
@@ -100,6 +103,7 @@ public class EnemyManager : MonoBehaviour
     void RushPattern()  //lookAtTimeになるまでプレイヤーの座標に向けて移動し、それ以降はその時向いていた方向に直線的に移動。プレイヤーZが手前に移動したとき非表示。
     {
         rushTimer += Time.deltaTime;
+        if (rushTimer < stopTime) return;
         if (rushTimer < lookAtTime)
         {
             transform.LookAt(player.transform);
