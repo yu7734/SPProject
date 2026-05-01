858933f3eb71e107d2171602494e63e5259cf677
lime <utida7497@gmail.com>
2026-05-01 10:16:11 +0900

    Add files via upload

--- EnemyManager.cs
+++ EnemyManager.cs
@@ -2,8 +2,13 @@ using UnityEngine;
 
 public class EnemyManager : MonoBehaviour
 {
-    [Header("number1->射撃,number2->突撃")]
-    public int enemyNumber = 1;
+    [Header("行動種類(複数選択可)")]
+    [Tooltip("通常弾")]
+    public bool isShot = false;
+    [Tooltip("自機狙いの弾")]
+    public bool isTrackShot = false;
+    [Tooltip("自機狙い突進")]
+    public bool isRushShot = false;
 
     [Header("射撃にかかわる変数")]
     public GameObject Bullet;
@@ -21,8 +26,11 @@ public class EnemyManager : MonoBehaviour
     public float RushOffset = 10f;
 
     private float offset = 1.5f; // 敵の大きさによって調整
-    private float timer = 0;
+    private float trackShotTimer = 0;
+    private float shotTimer = 0;
+    private float rushTimer = 0;
 
+    private float timer = 0;
     Transform player;   //プレイヤーの座標
     Rigidbody rb; 
 
@@ -44,20 +52,24 @@ public class EnemyManager : MonoBehaviour
     {
         if (player == null) return;
 
-        if (enemyNumber == 1)   //インスペクターで制御
+        if (isTrackShot)   //インスペクターで制御
+        {
+            TrackShotPattern();
+        }
+        if (isShot)
         {
             ShotPattern();
-        }   
-        if(enemyNumber == 2)
+        }
+        if (isRushShot)
         {
             RushPattern();
         }
     }
-    void ShotPattern()
+    void TrackShotPattern()
     {
-        transform.LookAt(player.transform); //プレイヤー側に常に向く
-        timer += Time.deltaTime;
-        if (timer > coolTime)   //coolTimeごとにoffsetの距離前に生成してその時点のプレイヤーの方向にenemyBulletForceの力で撃つ。３秒後に各自破壊。
+        if(!isRushShot) transform.LookAt(player.transform); //プレイヤー側に常に向く
+        trackShotTimer += Time.deltaTime;
+        if (trackShotTimer > coolTime)   //coolTimeごとにoffsetの距離前に生成してその時点のプレイヤーの方向にenemyBulletForceの力で撃つ。３秒後に各自破壊。
         {
             Vector3 spawnPosition = transform.position + transform.forward * offset;
 
@@ -68,13 +80,28 @@ public class EnemyManager : MonoBehaviour
             
             Bulletrb.AddForce(shotDirection * enemyBulletForce);
             Destroy(newBullet, 3f);
-            timer = 0;
+            trackShotTimer = 0;
+        }
+    }
+    void ShotPattern()
+    {
+
+        shotTimer += Time.deltaTime;
+        if (shotTimer > coolTime)   //coolTimeごとにoffsetの距離前に生成してその時点のプレイヤーの方向にenemyBulletForceの力で撃つ。３秒後に各自破壊。
+        {
+            Vector3 spawnPosition = transform.position + transform.forward * offset;
+
+            GameObject newBullet = Instantiate(Bullet, spawnPosition, transform.rotation);
+            Rigidbody Bulletrb = newBullet.GetComponent<Rigidbody>();
+            Bulletrb.AddForce(transform.forward * enemyBulletForce);
+            Destroy(newBullet, 3f);
+            shotTimer = 0;
         }
     }
     void RushPattern()  //lookAtTimeになるまでプレイヤーの座標に向けて移動し、それ以降はその時向いていた方向に直線的に移動。プレイヤーZが手前に移動したとき非表示。
     {
-        timer += Time.deltaTime;
-        if (timer < lookAtTime)
+        rushTimer += Time.deltaTime;
+        if (rushTimer < lookAtTime)
         {
             transform.LookAt(player.transform);
         }
