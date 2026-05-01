b4af373f2612c1f4f8d185281d47aba32a889b24
krmrhnm <krmrhnm130@gmail.com>
2026-05-01 11:06:59 +0900

    障害物のランダム生成
    2026/05/01/11:06

---
+++ Assets/Scripts/ObstacleManager.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class ObstacleManager : MonoBehaviour
+{
+    public int Stage = 1;//stagemanager等のscriptで管理する場合はそちらを参照する
+    public int limit = 5;//一度に存在できるObstacleの上限
+    public GameObject[] Obstacle1;//InspectorでObstacleのPrefabを追加する
+    public GameObject[] Obstacle2;//Stageを変化させるときにDestroy()で今存在しているObstacleを一掃したほうがいい
+    public GameObject[] Obstacle3;
+    float timer=0f;
+    float frequency = 1;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        switch (Stage)
+        {
+            default:
+            case 1:
+                GameObject[] tagobj = GameObject.FindGameObjectsWithTag("Obstacle");//Obstacleのついたobjectをtagobjに配列化
+                timer += Time.deltaTime;
+                if (timer >= frequency)//frequency秒毎にObstacleを生成する
+                {
+                    int i = Random.Range(0, Obstacle1.Length);
+                    int x = Random.Range(0, 5);
+                    int y = Random.Range(0, 5);
+                    int z = Random.Range(0, 5);
+                    if (tagobj.Length < limit) Instantiate(Obstacle1[i], new Vector3(x, y, z + this.transform.position.z), Quaternion.identity);//tagobjとlimitを比較、ランダムに呼び出し
+                    timer -= frequency;
+                }
+            break;
+        }
+    }
+}
