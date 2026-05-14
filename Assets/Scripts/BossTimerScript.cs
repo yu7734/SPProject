c092db79ca5fb0618aee5c4a305ab113447a47ee
krmrhnm <krmrhnm130@gmail.com>
2026-05-14 11:16:06 +0900

    2026/05/14/11:16
    StarScrollManager等のInstantiateの最後の部分にthis.transform.perentをつけてほしい
    詳しいことは聞きに来てくれ

---
+++ Assets/Scripts/BossTimerScript.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class BossTimerScript : MonoBehaviour
+{
+    [SerializeField] GameObject[] BossObject;
+    public int Stage = 1;//stagemanager“™‚Ìscript‚ÅŠÇ—‚·‚éê‡‚Í‚»‚¿‚ç‚ðŽQÆ‚·‚é
+    float Timer=0f;
+    float frequency = 0.1f;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Timer += Time.deltaTime;
+        if( Timer >= frequency)
+        {
+            Instantiate( BossObject[Stage-1],this.transform.position,Quaternion.identity);
+        }
+    }
+}
