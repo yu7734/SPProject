1c068d8a6fe793863486ce1343ef427c34c73e7c
krmrhnm <krmrhnm130@gmail.com>
2026-05-14 15:00:10 +0900

    BossTimerMagager
    仮のボス出現scriptとobject

--- Assets/Scripts/BossTimerScript.cs
+++ Assets/Scripts/BossTimerScript.cs
@@ -5,7 +5,8 @@ public class BossTimerScript : MonoBehaviour
     [SerializeField] GameObject[] BossObject;
     public int Stage = 1;//stagemanager“™‚Ìscript‚ÅŠÇ—‚·‚éê‡‚Í‚»‚¿‚ç‚ðŽQÆ‚·‚é
     float Timer=0f;
-    float frequency = 0.1f;
+    float frequency = 30f;
+    bool appearance = false;
     void Start()
     {
         
@@ -14,9 +15,10 @@ public class BossTimerScript : MonoBehaviour
     void Update()
     {
         Timer += Time.deltaTime;
-        if( Timer >= frequency)
+        if( Timer >= frequency&&!appearance)
         {
             Instantiate( BossObject[Stage-1],this.transform.position,Quaternion.identity);
+            appearance = true;
         }
     }
 }
