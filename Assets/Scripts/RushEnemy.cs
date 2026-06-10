7bdb8428b72659cb3e4984721cb66cb2a8ca5960
yu7734 <yuichi.7734@gmail.com>
2026-06-10 12:32:06 +0900

    リバート修正その２

--- Assets/Scripts/RushEnemy.cs
+++ Assets/Scripts/RushEnemy.cs
@@ -3,47 +3,26 @@ using UnityEngine;
 
 public class RushEnemy : EnemyAttackBase
 {
-    [Serializable]
-    public class ShotSetting
-    {
-        [Header("射撃にかかわる変数")]
-        public GameObject Bullet;
-        [Tooltip("自機狙いかどうか")]
-        public bool isTracking = false;
-        [Tooltip("弾を撃つ間隔（秒）")]
-        public float coolTime = 0.5f;
-        [Tooltip("弾を撃つ力（速さ）")]
-        public float enemyBulletForce = 3f;
-
-        [Tooltip("弾を最初に生成する場所")]
-        public float offset = 1.5f;
-    }
-
     [Header("突撃にかかわる変数")]
     [SerializeField,Tooltip("最初の停止時間（秒）")]
     private float stopTime = 1;
     [SerializeField, Tooltip("プレイヤーを見ている時間（秒）")]
     private float lookAtTime = 2;
-    [SerializeField,Tooltip("射撃するかどうか")]
-    private bool canShot = false;
+    [SerializeField, Tooltip("敵の突撃速度")]
+    private float enemyRushSpeed = 3f;
 
     private float rushTimer = 0;
-    [NonSerialized] public float shotTimer = 0;
-
-    [SerializeField] private ShotSetting shot = new();
 
     private void Update()
     {
 
         if (player == null) return;
         RushPattern();
-        if(canShot)ShotPattern();
     }
     public override void OnReset()
     {
         base.OnReset();
         rushTimer = 0;
-        if (canShot) shotTimer = 0;
     }
     public void RushPattern()  //lookAtTimeになるまでプレイヤーの座標に向けて移動し、それ以降はその時向いていた方向に直線的に移動。プレイヤーZが手前に移動したとき非表示。
     {
@@ -53,30 +32,7 @@ public class RushEnemy : EnemyAttackBase
         {
             transform.LookAt(player.transform);
         }
-        transform.position += transform.forward * enemyMoveSpeed * Time.deltaTime;
+        transform.position += transform.forward * enemyRushSpeed * Time.deltaTime;
 
-    }
-    public void ShotPattern() {
-        shotTimer += Time.deltaTime;
-        if (shotTimer > shot.coolTime)   //coolTimeごとにoffsetの距離前に生成してその時点のプレイヤーの方向にenemyBulletForceの力で撃つ。３秒後に各自破壊。
-        {
-            Vector3 spawnPosition = transform.position + transform.forward * shot.offset;
-
-            GameObject newBullet = Instantiate(shot.Bullet, spawnPosition, transform.rotation);
-            Rigidbody Bulletrb = newBullet.GetComponent<Rigidbody>();
-
-            if (shot.isTracking)
-            {
-                Vector3 shotDirection = (player.position - spawnPosition).normalized;
-                Bulletrb.AddForce(-shotDirection * -shot.enemyBulletForce);
-            }
-            else
-            {
-                Bulletrb.AddForce(-transform.forward * shot.enemyBulletForce);
-            }
-
-            Destroy(newBullet, 3f);
-            shotTimer = 0;
-        }
     }
 }
