faef651e81332be437db733b676d7d15770e35cb
lime7497 <utida7497@gmail.com>
2026-06-10 13:02:01 +0900

    enemy

---
+++ Assets/Scripts/EnemyAimingLaser.cs
@@ -0,0 +1,82 @@
+﻿using Unity.VisualScripting;
+using UnityEngine;
+
+[RequireComponent(typeof(EnemyManager))]
+public class EnemyAimingLaser : EnemyAttackBase
+{
+    [SerializeField,Header("照準レーザーにかかわる変数")]
+    private GameObject Laser;
+    [SerializeField, Tooltip("初期停止時間")]
+    private float stopTime;
+    [SerializeField, Tooltip("照準時間")]
+    private float aimingTime;
+    [SerializeField, Tooltip("レーザーを撃つ時間")]
+    private float laserTimeLimit;
+    [SerializeField, Tooltip("弾を撃つ力（速さ）")]
+    private float enemyBulletForce;
+    [SerializeField, Tooltip("弾を最初に生成する場所")]
+    private float offset;
+
+    private float timer;
+    private float laserTimer;
+    private LineRenderer lineRenderer;
+
+    private void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.positionCount = 2;
+        lineRenderer.enabled = false;
+    }
+    // Update is called once per frame
+    private void Update()
+    {
+        Aiming();
+        
+        Debug.Log(laserTimer);
+    }
+    public void Aiming()
+    {
+        timer += Time.deltaTime;
+
+        if (timer < stopTime)
+        {
+            transform.LookAt(player.transform);
+            return;
+        }
+        LaserPatturn();
+
+    }
+    public void LaserPatturn()
+    {
+        if (timer < stopTime + aimingTime && timer > stopTime)
+        {
+            transform.LookAt(player.transform);
+
+            lineRenderer.enabled = true;
+            lineRenderer.SetPosition(0, transform.position);
+            lineRenderer.SetPosition(1, player.position);
+
+            return;
+        }
+        float chargeTime = 0;
+        chargeTime += Time.deltaTime;
+        if (chargeTime < 1) return;
+
+        laserTimer += Time.deltaTime;
+        lineRenderer.enabled = false;
+        Vector3 spawnPosition = transform.position + transform.forward * offset;
+
+        GameObject newBullet = Instantiate(Laser, spawnPosition, transform.rotation);
+        Rigidbody Bulletrb = newBullet.GetComponent<Rigidbody>();
+
+        Bulletrb.AddForce(-transform.forward * -enemyBulletForce);
+        Destroy(newBullet, 3f);
+
+        if (laserTimer > laserTimeLimit)
+        {
+            timer = 0;
+            laserTimer = 0;
+        }
+    }
+    
+}
---
+++ Assets/Scripts/EnemyRush.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(EnemyManager))]
+public class EnemyRush : EnemyAttackBase
+{
+    [Header("突撃にかかわる変数")]
+    [SerializeField,Tooltip("最初の停止時間（秒）")]
+    private float stopTime = 1;
+    [SerializeField, Tooltip("プレイヤーを見ている時間（秒）")]
+    private float lookAtTime = 2;
+    [SerializeField, Tooltip("敵の突撃速度")]
+    private float enemyRushSpeed = 3f;
+
+    private float rushTimer = 0;
+
+    private void Update()
+    {
+
+        if (player == null) return;
+        RushPattern();
+    }
+    public override void OnReset()
+    {
+        base.OnReset();
+        rushTimer = 0;
+    }
+    /// <summary>
+    /// rushTimer &lt; lookAtTimeでプレイヤーの座標に向けて移動し、 &gt; lookAtTimeではその時向いていた方向に直線的に移動
+    /// </summary>
+    public void RushPattern()  //lookAtTimeになるまでプレイヤーの座標に向けて移動し、それ以降はその時向いていた方向に直線的に移動。プレイヤーZが手前に移動したとき非表示。
+    {
+        rushTimer += Time.deltaTime;
+        if (rushTimer < stopTime) return;
+        if (rushTimer < lookAtTime)
+        {
+            transform.LookAt(player.transform);
+        }
+        transform.position += transform.forward * enemyRushSpeed * Time.deltaTime;
+
+    }
+}
