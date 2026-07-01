cdb710810bb5586913bb065a7fa4027d8159a5ad
krmrhnm <krmrhnm130@gmail.com>
2026-07-01 12:18:26 +0900

    SyabonItemの追加

--- Assets/Scripts/ItemScrips/LaserCannon/CannonLaserScript.cs
+++ Assets/Scripts/ItemScrips/LaserCannon/CannonLaserScript.cs
@@ -3,7 +3,7 @@ using UnityEngine.UIElements;
 
 public class CannonLaserScript : MonoBehaviour
 {
-    [SerializeField] float RotateSpeed = 360f;
+    [SerializeField,Tooltip("1•bŠÔ‚É‰½“x‰ñ“]‚·‚é‚©")] float RotateSpeed = 360f;
     void Update()
     {
         gameObject.transform.Rotate(0, 0, RotateSpeed*Time.deltaTime);
