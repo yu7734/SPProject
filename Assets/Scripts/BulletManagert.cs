6c3c6c05dc609b28ec37b45fe29d4f35c4dac3dc
krmrhnm <krmrhnm130@gmail.com>
2026-07-07 13:49:54 +0900

    BulletManagertChange

--- Assets/Scripts/BulletManagert.cs
+++ Assets/Scripts/BulletManagert.cs
@@ -5,13 +5,15 @@ public class BulletManagert : MonoBehaviour
     Rigidbody rb;
     [SerializeField] private float bulletSpeed;
     [SerializeField] public static int bulletPower = 5;
-
+    [Tooltip("ダメージ倍率")]public float bulletDamageRate = 1.0f;
+    [Tooltip("倍率計算後に固定値加算")]public int bulletDamageBonus = 0;
+    int bulletAttack;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         //RigidBody���擾���A�v���C���[�̌����Ă�������ɒe���΂�
         rb = GetComponent<Rigidbody>(); 
-        rb.linearVelocity = this.transform.forward * bulletSpeed * Time.deltaTime;
+        rb.linearVelocity = this.transform.forward * bulletSpeed * Time.fixedDeltaTime;
     }
 
     // Update is called once per frame
@@ -24,10 +26,11 @@ public class BulletManagert : MonoBehaviour
     {
         if (other.CompareTag(this.tag)) return;
         IEnemyDamage damage = other.gameObject.GetComponentInParent<IEnemyDamage>();
-
+        bulletAttack = (int)(bulletPower * bulletDamageRate) + bulletDamageBonus;
+        Debug.Log(bulletAttack);
         if (damage != null)
         {
-            damage.EnemyDamaged(bulletPower);
+            damage.EnemyDamaged(bulletAttack);
             Destroy(gameObject);
         }
     }
