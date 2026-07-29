265df5a8b0408292738ec8a0a577b41f4f9679f4
lime7497 <utida7497@gmail.com>
2026-07-29 11:19:37 +0900

    軽い修正

--- Assets/Scripts/ItemScrips/Heal/PlayerHealScript.cs
+++ Assets/Scripts/ItemScrips/Heal/PlayerHealScript.cs
@@ -7,12 +7,10 @@ public class PlayerHealScript : MonoBehaviour
     public float UntilIncreaseHealing = 30f;
     /// <summary> 一定時間毎の回復量の増加量 </summary>
     public float HealingRate = 5f;
-    PlayerManager manager;
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            //manager=other.GetComponentInParent<PlayerManager>();
             int TotalHealing = Healing + (int)(Time.timeSinceLevelLoad/ UntilIncreaseHealing * HealingRate);
             if (other.TryGetComponent<IPlayerHeal>(out var Heal)) Heal.Heal(TotalHealing);
             Destroy(gameObject);
